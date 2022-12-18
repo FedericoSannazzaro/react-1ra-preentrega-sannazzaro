73907868d9b2d2a1e6ca0a67da0a73afd5b836e3
Federico Sannazzaro <federico.sannazzaro@hotmail.com>
2022-12-18 19:14:48 -0300

    agregamos NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+export const NavBar = () => {
+    return (
+
+        <div className="container">
+            <nav className="nav">
+                <div className="nav_brand">
+                    <a className="nav_link" href="#">Mi Logo</a>
+                </div>
+                <ul className="nav_list">
+                    <li>
+                        <a className="nav_link" href="#">Categoria 1</a>
+                    </li>
+                    <li>
+                        <a className="nav_link" href="#">Categoria 2</a>
+                    </li>
+                    <li>
+                        <a className="nav_link" href="#">Mi Carrito</a>
+                    </li>
+
+
+                </ul>
+            </nav>
+        </div>
+
+
+    );
+}
+
+export default NavBar;
